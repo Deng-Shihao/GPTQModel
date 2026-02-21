4e6cea5955ec5afe45589d51bdc16ebe697f068d
Shihao <sd24191@essex.ac.uk>
2026-02-21 18:02:19 +0000

    fix setup.py problem

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 uv venv -p 3.12 && source .venv/bin/activate
 
-uv pip install setuptools
+uv pip install setuptools torch
+
+uv pip install --upgrade pip setuptools wheel
 
 uv pip install -v . --no-build-isolation
