e1c9b3e6f400a3942dfa69401325e1dd18025624
Shihao <sd24191@essex.ac.uk>
2026-02-21 17:25:56 +0000

    add awq example

---
+++ awq_example.py
@@ -0,0 +1,21 @@
+from datasets import load_dataset
+from gptqmodel import GPTQModel, QuantizeConfig
+from gptqmodel.quantization import FORMAT, METHOD
+
+model_id = "Qwen/Qwen3-4B-Thinking-2507"
+quant_path = "./quantized_models/Qwen3-4B-Thinking-2507-awq-4bit"
+
+calibration_dataset = load_dataset(
+    "allenai/c4",
+    data_files="en/c4-train.00001-of-01024.json.gz",
+    split="train"
+  ).select(range(1024))["text"]
+
+quant_config = QuantizeConfig(bits=4, group_size=128, quant_method=METHOD.AWQ)
+
+model = GPTQModel.load(model_id, quant_config)
+
+# increase `batch_size` to match gpu/vram specs to speed up quantization
+model.quantize(calibration_dataset, batch_size=2)
+
+model.save(quant_path)
